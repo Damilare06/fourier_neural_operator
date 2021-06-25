37f8695208974a2e2c45fc192d2193e370d7cb71
Damilare06 <adesojidiekola@gmail.com>
2021-06-25 13:59:48 -0400

    can compare the output images

--- FNO_1d_attack.py
+++ FNO_1d_attack.py
@@ -1,15 +1,28 @@
-""" Simple attack showing the Burgers 1d model attack for different epsolons vs accuracy"""
+""" 
+    Simple attack showing the Burgers 1d model attack for different epsolons vs accuracy
+    Run with "python3.8 single_attack.py"
+"""
 
-# run with python3.8 single_attack_foolbox.py 
-from numpy import exp
 import torch
 import torchvision.models as models
-import eagerpy as ep
 import torch.nn.functional as F
 from fourier_1d_module import * 
 from utilities3 import *
 
-def mse_attack(model, X, y, epsilon=0.02, alpha=1e-3, num_iter=40):
+def mse_attack(model, X, y, epsilon=0.1, alpha=1e-5, num_iter=1):
+    """ 
+        Construct PGD-like MSE attacks on X with minimal default values"
+    """
+    delta = torch.zeros_like(X, requires_grad=True)
+
+    for t in range(num_iter):
+        loss = F.mse_loss(model(X + delta).squeeze(), y)
+        loss.backward()
+        delta.data = (delta + X.shape[0] * alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
+        delta.grad.zero_()
+    return delta.detach()
+
+def mse_linf_attack(model, X, y, epsilon=0.02, alpha=1e-3, num_iter=40):
     """ 
         Construct PGD-like MSE attacks on X"
     """
@@ -18,22 +31,94 @@ def mse_attack(model, X, y, epsilon=0.02, alpha=1e-3, num_iter=40):
     for t in range(num_iter):
         loss = F.mse_loss(model(X + delta).squeeze(), y)
         loss.backward()
-        delta.data = (delta + X.shape[0]*alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon) #TODO: migth have to change this
+        delta.data = (delta + alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
         delta.grad.zero_()
     return delta.detach()
 
 
-def main() -> None:
+def mse_linf_rand_attack(model, X, y, epsilon, alpha, num_iter, restarts):
+    max_loss = torch.zeros(y.shape[0]).cuda()
+    max_delta = torch.zeros_like(X.squeeze())
+    """ 
+        Construct PGD-like MSE attacks on X"
+    """
+    for i in range(restarts):
+        delta = torch.rand_like(X.squeeze(), requires_grad=True)
+        delta.data = delta.data * 2 * epsilon - epsilon
 
+        for t in range(num_iter):
+            loss = F.mse_loss(model(X + delta).squeeze(), y)
+            loss.backward()
+            delta.data = (delta + alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
+            delta.grad.zero_()
+
+        all_loss = F.mse_loss(model(X + delta).squeeze(), y, reduction='none')            
+        max_delta[all_loss >= max_loss] = delta.detach()[all_loss >= max_loss]
+        max_loss = torch.max(max_loss, all_loss)
+
+    return max_delta
+
+
+def epoch_adversarial(model, loader, attack, attack_name, *args):
+    total_loss = 0.
+    for X, y in loader:
+        X, y = X.cuda(), y.cuda()
+        delta = attack(model, X, y.squeeze(), *args)
+        yp = model(X + delta)
+        loss = F.mse_loss(yp.squeeze(), y.squeeze())
+        
+        total_loss += loss
+
+    output =  total_loss / len(loader.dataset)
+    print(f"{attack_name} error: {output :.6f} ")
+    return output
+
+def show_burgers_overlap(var1, var2, key1, key2):
+    cm = plt.cm.get_cmap('viridis')
+    var = [var1.numpy(), var2.numpy()]
+    var = np.asarray(var)
+    var = var.reshape(var.shape[1], var.shape[2], var.shape[0])
+    print(var.shape)
+    key = [key1, key2]
+
+    # Generate the data distribution
+    fig, ax = plt.subplots()
+    ax.set_title(f"N = {var.shape[0]} of {key[0]} vs {key[1]} on {var.shape[1]} grid\n")
+    var_hist = var.reshape(var.shape[0] * var.shape[1], var.shape[2])
+    print(var_hist.shape)
+    ax.hist(var_hist, density=True, stacked=False, bins='auto', label=key)
+    ax.set_xlabel(f"{key} distribution")
+    ax.legend()
+
+def show_burgers(var, key):
+    cm = plt.cm.get_cmap('viridis')
+    var = var.numpy()
+
+    # Generate the data distribution
+    fig, ax = plt.subplots()
+    ax.set_title(f"N = {var.shape[0]} of {key} on {var.shape[1]} grid\n")
+    var_hist = var.flatten()
+    n, bins, patches = ax.hist(var_hist, density=True, stacked=True, bins='auto', color="green")
+    ax.set_xlabel(f"{key} distribution")
+    bin_centers = 0.5 * (bins[:-1] + bins[1:])
+
+    # scale values to interval [0,1]
+    col = bin_centers - min(bin_centers)
+    col /= max(col)
+
+    for c, p in zip(col, patches):
+        plt.setp(p, 'facecolor', cm(c))    
+    plt.show()
+
+def main() -> None:
     #  configurations
     ntest = 100
-
     sub = 2**3 # subsampling rate
     h = 2**13 // sub # total grid size divided by the subsampling rate
     s = h
 
     batch_size = 1 # 20
-    debug = True
+    debug = False 
 
     # read data
     # ... of the shape (number of samples, grid size)
@@ -55,9 +140,6 @@ def main() -> None:
     model = torch.load('model/ns_fourier_burgers').eval()
 
     # Evaluation - the foolbox accuracy loss is for classification
-    # myloss = LpLoss(size_average=False)
-
-    # test_l2 = 0.0
     pred = torch.zeros(y_test.shape)
     index = 0
     test_mse = 0
@@ -68,33 +150,27 @@ def main() -> None:
             out = model(x)
             pred[index] = out.squeeze()
 
-            # test_l2 += myloss(out.view(1, -1), y.view(1, -1)).item()
             mse = F.mse_loss(out.view(batch_size, -1), y.view(batch_size, -1), reduction='mean')            
             test_mse += mse.item()
 
             index = index + 1
-    # test_l2 /= ntest
-    # print(f"test_l2 loss: {test_l2 * 100 :.4f} %")
     test_mse /= ntest
+    # show_burgers(y_test, 'y_test')
+    # show_burgers(pred, 'y_pred')
+    show_burgers_overlap(y_test, pred, 'y_test', 'y_pred')
+    plt.show()
     print(f"test_mse loss before attack: {test_mse :.6f} ")
 
-    # TODO: Perturb the input (delta must < epsilon) and Train the MSE Loss on it, clip afterward
-    x_test, y_test = x_test.cuda(), y_test.cuda()
-    delta = mse_attack(model, x_test, y_test, epsilon=0.1, alpha=1e4, num_iter=500)
-    if debug:
-        print("y_test type: ", type(y_test)," and shape:", (y_test).shape)
-        print("delta: ", type(delta), (delta).shape)
+    # Comment: alpha = step size, epsilon = perturbation range 
+    eps = 0.1
+    num_iter = 10
+    restarts = 10
+    alpha = eps/ num_iter
 
-    y_pred = model(x_test + delta).squeeze()
-    pert_err = F.mse_loss(y_pred, y_test, reduction='mean')
-    print(f"perturbed mse error: {pert_err :.4f} ")
+    # epoch_adversarial(model, test_loader, mse_attack, "mse_attack", eps, alpha, num_iter)
+    # epoch_adversarial(model, test_loader, mse_linf_rand_attack, "mse_linf_rand_attack", eps, alpha, num_iter, restarts)
+    # epoch_adversarial(model, test_loader, mse_linf_attack, "mse_linf_attack", eps, alpha, num_iter)
 
-    # # Automatically compute the robust accuracy
-    # robust_accuracy = 1 - success.float32().mean(axis=-1)
-    # print(type(robust_accuracy))
-    # print("robust accuracy for perturbations with")
-    # for eps, acc in zip(epsilons, robust_accuracy):
-    #     print(f" Linf norm <= {eps: < 6}: {acc.item() * 100:4.1f} %")
 
 
 if __name__ == "__main__":
--- FNO_attack.py
+++ FNO_attack.py
@@ -1,9 +1,359 @@
-# its inherits from BaseGradientDescent
-from foolbox.distances import linf
-from foolbox.attacks.gradient_descent_base import LinfBaseGradientDescent, BaseGradientDescent
+"""
+L2AdditiveUniformNoiseAttackRegr - similar to the noise addition for classification
+    It depends on the L2Mixin, GaussianMixin and BaseAdditiveNoiseAttack classes
 
-class FNO_LinfPGD(BaseGradientDescent):
-    distance = linf
+    the suffix ...Regr is used to denote routines to be modified for Regr
+"""
+# its inherits from BaseGradientDescent
+from typing import Callable, TypeVar, Any, Union, Optional, Sequence, List, Tuple, Dict
+
+from foolbox.distances import Distance, l2
+from foolbox.attacks.gradient_descent_base import LinfBaseGradientDescent, BaseGradientDescent
+from foolbox.attacks.base import AttackWithDistance, Criterion
+from foolbox.attacks.additive_noise import L2Mixin,UniformMixin
+from foolbox.models.base import Model
+from typing_extensions import final, overload
+from abc import ABC, abstractmethod
+from collections.abc import Iterable
+import eagerpy as ep
+
+T = TypeVar("T")
+
+# class Attack(ABC):
+class AttackRegr(ABC):
+    @overload
+    def __call__(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Sequence[Union[float, None]],
+        **kwargs: Any,
+    ) -> Tuple[List[T], List[T], T]:
+        ...
+
+    @overload  # noqa: F811
+    def __call__(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Union[float, None],
+        **kwargs: Any,
+    ) -> Tuple[T, T, T]:
+        ...
+
+    @abstractmethod  # noqa: F811
+    def __call__(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Union[Sequence[Union[float, None]], float, None],
+        **kwargs: Any,
+    ) -> Union[Tuple[List[T], List[T], T], Tuple[T, T, T]]:
+        # in principle, the type of criterion is Union[Criterion, T]
+        # but we want to give subclasses the option to specify the supported
+        # criteria explicitly (i.e. specifying a stricter type constraint)
+        ...
+
+    @abstractmethod
+    def repeat(self, times: int) -> "Attack":
+        ...
+
+    def __repr__(self) -> str:
+        args = ", ".join(f"{k.strip('_')}={v}" for k, v in vars(self).items())
+        return f"{self.__class__.__name__}({args})"
+
+
+# class AttackWithDistance(Attack):
+class AttackWithDistanceRegr(AttackRegr):
+    @property
+    @abstractmethod
+    def distance(self) -> Distance:
+        ...
+
+    def repeat(self, times: int) -> AttackRegr:
+        return Repeated(self, times)
+
+# class Repeated(AttackWithDistance):
+class Repeated(AttackWithDistanceRegr):
+    """Repeats the wrapped attack and returns the best result"""
+
+    def __init__(self, attack: AttackWithDistance, times: int):
+        if times < 1:
+            raise ValueError(f"expected times >= 1, got {times}")  # pragma: no cover
+
+        self.attack = attack
+        self.times = times
+
+    @property
+    def distance(self) -> Distance:
+        return self.attack.distance
+
+    @overload
+    def __call__(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Sequence[Union[float, None]],
+        **kwargs: Any,
+    ) -> Tuple[List[T], List[T], T]:
+        ...
+
+    @overload  # noqa: F811
+    def __call__(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Union[float, None],
+        **kwargs: Any,
+    ) -> Tuple[T, T, T]:
+        ...
+
+    def __call__(  # noqa: F811
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Union[Sequence[Union[float, None]], float, None],
+        **kwargs: Any,
+    ) -> Union[Tuple[List[T], List[T], T], Tuple[T, T, T]]:
+        x, restore_type = ep.astensor_(inputs)
+        del inputs
+
+        criterion = get_criterion(criterion)
+
+        was_iterable = True
+        if not isinstance(epsilons, Iterable):
+            epsilons = [epsilons]
+            was_iterable = False
+
+        N = len(x)
+        K = len(epsilons)
+
+        for i in range(self.times):
+            # run the attack
+            xps, xpcs, success = self.attack(
+                model, x, criterion, epsilons=epsilons, **kwargs
+            )
+            assert len(xps) == K
+            assert len(xpcs) == K
+            for xp in xps:
+                assert xp.shape == x.shape
+            for xpc in xpcs:
+                assert xpc.shape == x.shape
+            assert success.shape == (K, N)
+
+            if i == 0:
+                best_xps = xps
+                best_xpcs = xpcs
+                best_success = success
+                continue
+
+            # TODO: test if stacking the list to a single tensor and
+            # getting rid of the loop is faster
+
+            for k, epsilon in enumerate(epsilons):
+                first = best_success[k].logical_not()
+                assert first.shape == (N,)
+                if epsilon is None:
+                    # if epsilon is None, we need the minimum
+
+                    # TODO: maybe cache some of these distances
+                    # and then remove the else part
+                    closer = self.distance(x, xps[k]) < self.distance(x, best_xps[k])
+                    assert closer.shape == (N,)
+                    new_best = ep.logical_and(success[k], ep.logical_or(closer, first))
+                else:
+                    # for concrete epsilon, we just need a successful one
+                    new_best = ep.logical_and(success[k], first)
+                new_best = atleast_kd(new_best, x.ndim)
+                best_xps[k] = ep.where(new_best, xps[k], best_xps[k])
+                best_xpcs[k] = ep.where(new_best, xpcs[k], best_xpcs[k])
+
+            best_success = ep.logical_or(success, best_success)
+
+        best_xps_ = [restore_type(xp) for xp in best_xps]
+        best_xpcs_ = [restore_type(xpc) for xpc in best_xpcs]
+        if was_iterable:
+            return best_xps_, best_xpcs_, restore_type(best_success)
+        else:
+            assert len(best_xps_) == 1
+            assert len(best_xpcs_) == 1
+            return (
+                best_xps_[0],
+                best_xpcs_[0],
+                restore_type(best_success.squeeze(axis=0)),
+            )
+
+    def repeat(self, times: int) -> "Repeated":
+        return Repeated(self.attack, self.times * times)
+
+
+
+
+
+# class FixedEpsilonAttackRegr(AttackWithDistance):
+class FixedEpsilonAttackRegr(AttackWithDistanceRegr):
+    """Fixed-epsilon attacks try to find adversarials whose perturbation sizes
+    are limited by a fixed epsilon"""
+
+    @abstractmethod
+    def run(
+        self, model: Model, inputs: T, criterion: Any, *, epsilon: float, **kwargs: Any
+    ) -> T:
+        """Runs the attack and returns perturbed inputs.
+        The size of the perturbations should be at most epsilon, but this
+        is not guaranteed and the caller should verify this or clip the result.
+        """
+        ...
+
+    @overload
+    def __call__(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Sequence[Union[float, None]],
+        **kwargs: Any,
+    ) -> Tuple[List[T], List[T], T]:
+        ...
+
+    @overload  # noqa: F811
+    def __call__(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Union[float, None],
+        **kwargs: Any,
+    ) -> Tuple[T, T, T]:
+        ...
+
+    @final  # noqa: F811
+    def __call__(  # type: ignore
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Any,
+        *,
+        epsilons: Union[Sequence[Union[float, None]], float, None],
+        **kwargs: Any,
+    ) -> Union[Tuple[List[T], List[T], T], Tuple[T, T, T]]:
+
+        x, restore_type = ep.astensor_(inputs)
+        del inputs
+
+        criterion = get_criterion(criterion)
+        is_adversarial = get_is_adversarial(criterion, model)
+
+        was_iterable = True
+        if not isinstance(epsilons, Iterable):
+            epsilons = [epsilons]
+            was_iterable = False
+
+        N = len(x)
+        K = len(epsilons)
+
+        # None means: just minimize, no early stopping, no limit on the perturbation size
+        if any(eps is None for eps in epsilons):
+            # TODO: implement a binary search
+            raise NotImplementedError(
+                "FixedEpsilonAttack subclasses do not yet support None in epsilons"
+            )
+        real_epsilons = [eps for eps in epsilons if eps is not None]
+        del epsilons
+
+        xps = []
+        xpcs = []
+        success = []
+        for epsilon in real_epsilons:
+            xp = self.run(model, x, criterion, epsilon=epsilon, **kwargs)
+
+            # clip to epsilon because we don't really know what the attack returns;
+            # alternatively, we could check if the perturbation is at most epsilon,
+            # but then we would need to handle numerical violations;
+            xpc = self.distance.clip_perturbation(x, xp, epsilon)
+            is_adv = is_adversarial(xpc)
+
+            xps.append(xp)
+            xpcs.append(xpc)
+            success.append(is_adv)
+
+        success_ = ep.stack(success)
+        assert success_.shape == (K, N)
+
+        xps_ = [restore_type(xp) for xp in xps]
+        xpcs_ = [restore_type(xpc) for xpc in xpcs]
+
+        if was_iterable:
+            return xps_, xpcs_, restore_type(success_)
+        else:
+            assert len(xps_) == 1
+            assert len(xpcs_) == 1
+            return xps_[0], xpcs_[0], restore_type(success_.squeeze(axis=0))
+
+
+class BaseAdditiveNoiseAttackRegr(FixedEpsilonAttackRegr, ABC):
+    def run(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Union[Criterion, Any] = None,
+        *,
+        epsilon: float,
+        **kwargs: Any,
+    ) -> T:
+        raise_if_kwargs(kwargs)
+        x, restore_type = ep.astensor_(inputs)
+        del inputs, criterion, kwargs
+
+        min_, max_ = model.bounds
+        p = self.sample_noise(x)
+        epsilons = self.get_epsilons(x, p, epsilon, min_=min_, max_=max_)
+        x = x + epsilons * p
+        x = x.clip(min_, max_)
+
+        return restore_type(x)
+
+    @abstractmethod
+    def sample_noise(self, x: ep.Tensor) -> ep.Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_epsilons(
+        self, x: ep.Tensor, p: ep.Tensor, epsilon: float, min_: float, max_: float
+    ) -> ep.Tensor:
+        raise NotImplementedError
+
+
+class L2Mixin:
+    distance = l2
+
+    def get_epsilons(
+        self, x: ep.Tensor, p: ep.Tensor, epsilon: float, min_: float, max_: float
+    ) -> ep.Tensor:
+        norms = flatten(p).norms.l2(axis=-1)
+        return epsilon / atleast_kd(norms, p.ndim)
+
+
+class L2AdditiveUniformNoiseAttackRegr(L2Mixin, UniformMixin, BaseAdditiveNoiseAttackRegr):
+    """Samples uniform noise with a fixed L2 size."""
+
+    pass
+
+
 
 # class LinfBaseGradientDescent(BaseGradientDescent):
 #     distance = linf
